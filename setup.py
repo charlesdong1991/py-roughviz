3c75c866232529aadea7870d0adee7978eb9dbb8
Kaiqi <kaiqi@kth.se>
2019-12-26 22:15:13 +0100

    add setup py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+__keywords__ = ["py-roughviz", "visualization", "sketchy charts", "hand-drawn charts"]
+
+setup(
+    name="py-roughviz",
+    description="A Python implementation of JavaScript Library RoughViz to create sketchy charts.",
+    version="0.1.0",
+    author="Kaiqi Dong",
+    author_email="kaiqidong1991@gmail.com",
+    licnese="MIT",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["jinja2", "ipython"],
+    tests_require=["pytest"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries",
+    ]
+)
