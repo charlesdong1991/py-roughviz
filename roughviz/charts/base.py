116b39db60cb8580b316be4feee410967304d171
Kaiqi <kaiqi@kth.se>
2019-12-27 13:38:52 +0100

    Add docstring for base class

--- roughviz/charts/base.py
+++ roughviz/charts/base.py
@@ -11,6 +11,9 @@ DATA_KEYS = {"labels", "values"}
 
 
 class BaseChart(RenderEngine):
+    """
+    This is the base class where all charts are inherited from
+    """
     BASE_KWARGS = {
         "title": "title",
         "interactive": "interactive",
@@ -28,19 +31,19 @@ class BaseChart(RenderEngine):
     def __init__(
         self,
         data,
-        values=None,
-        labels=None,
+        values: Optional[str] = None,
+        labels: Optional[str] = None,
         title: Optional[str] = None,
-        title_fontsize=0.95,
-        width=800,
-        height=600,
-        interactive=True,
-        bowing=0.2,
-        fill_style="cross-hatch",
-        fill_weight=0,
-        stroke_width=1,
-        roughness=1,
-        tooltip_fontsize=0.95,
+        title_fontsize: Optional[int, float] = 0.95,
+        width: Optional[int] = 800,
+        height: Optional[int] = 600,
+        interactive: bool = True,
+        bowing: Optional[float] = 0.2,
+        fill_style: Optional[str] = "cross-hatch",
+        fill_weight: Optional[int, float] = 0,
+        stroke_width: Optional[int, float] = 1,
+        roughness: Optional[int, float] = 1,
+        tooltip_fontsize: Optional[int, float] = 0.95,
         **kwargs,
     ):
         super().__init__()
@@ -65,6 +68,9 @@ class BaseChart(RenderEngine):
         self.opts["titleFontSize"] = title_fontsize
 
     def render_to_tmpl(self):
+        """This method is used to render the content to the template
+        """
+        # Assign an id for container.
         self.element_id = uuid.uuid4().hex
         self.opts["element"] = "#" + self.element_id
 
@@ -73,6 +79,8 @@ class BaseChart(RenderEngine):
 
     @staticmethod
     def _check_input_data(data):
+        """Check the input data type and format.
+        """
         if not isinstance(data, (str, dict)):
             raise TypeError("Only valid type of data is str and dictionary.")
         elif isinstance(data, str) and not data.endswith(DATA_TYPE):
@@ -83,6 +91,8 @@ class BaseChart(RenderEngine):
             )
 
     def _assign_input_values(self, labels, values):
+        """Assign the values for input data.
+        """
         if isinstance(self.data, str) and (not labels or not values):
             raise ValueError(
                 "You need to specify labels and values as separate" "attributes."
@@ -105,29 +115,53 @@ class BaseChart(RenderEngine):
 
     @staticmethod
     def _fontsize_converter(size):
+        """Convert the fontsize if it is numeric. Since the unit of fontsize in roughviz is `rem`."""
         if isinstance(size, Number):
             return str(size) + "rem"
         return size
 
-    def set_title(self, title, fontsize=None):
+    def set_title(self, title: str, fontsize: Optional[int, float] = None):
+        """Set the title for plots.
+
+        Args:
+            title: the title of the plot
+            fontsize: the fontsize of the title, if None, use the default size.
+        """
         self.opts["title"] = self._xstr(title)
         if fontsize:
             self.opts["titleFontSize"] = fontsize
         return self
 
-    def set_xlabel(self, xlabel, fontsize=None):
+    def set_xlabel(self, xlabel: str, fontsize: Optional[int, float] = None):
+        """Set the xlabel for plots.
+
+        Args:
+            xlabel: the xlabel of the plot
+            fontsize: the fontsize of the xlabel, if None, use the default size.
+        """
         self.opts["xLabel"] = self._xstr(xlabel)
         if fontsize:
             self.opts["labelFontSize"] = fontsize
         return self
 
-    def set_ylabel(self, ylabel, fontsize=None):
+    def set_ylabel(self, ylabel: str, fontsize: Optional[int, float] = None:
+        """Set the ylabel for plots.
+
+        Args:
+            ylabel: the ylabel of the plot
+            fontsize: the fontsize of the ylabel, if None, use the default size.
+        """
         self.opts["yLabel"] = self._xstr(ylabel)
         if fontsize:
             self.opts["labelFontSize"] = fontsize
         return self
 
-    def set_figsize(self, figsize):
+    def set_figsize(self, figsize: tuple):
+        """Set the figsize for plots, in a format of (width, height).
+
+        Args:
+            figsize: the figsize of the plot
+        """
         if not isinstance(figsize, tuple):
             raise ValueError("figsize has to be a tuple.")
         self.opts["width"] = figsize[0]
@@ -135,14 +169,23 @@ class BaseChart(RenderEngine):
 
         return self
 
-    def set_legend(self, legend=True, legend_position="right"):
+    def set_legend(self, legend: bool = True, legend_position: Optional[str] = "right"):
+        """Set the legend for plots.
+
+        Args:
+            legend: if having legend or not, default is True.
+            legend_position: the fontsize of the title, default is right.
+        """
         if legend:
             self.opts["legendPosition"] = legend_position
         else:
             self.opts["legend"] = legend
         return self
 
-    def _set_kwargs(self, mapping=None, **kwargs):
+    def _set_kwargs(self, mapping: Optional[dict] = None, **kwargs):
+        """Making a mapping for keyword arguments due to syntax difference and assign corresponding
+        values to mapped keywords.
+        """
         if mapping is None:
             mapping = self.BASE_KWARGS
         for kw, kw_value in kwargs.items():
