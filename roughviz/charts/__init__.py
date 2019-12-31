b3ff9c6a2685577741f1e650b00cc7ffaad961ab
Kaiqi <kaiqi@kth.se>
2019-12-31 23:40:50 +0100

    Make plots import easier

---
+++ roughviz/charts/__init__.py
@@ -0,0 +1,7 @@
+from ..charts.bar import Bar
+from ..charts.barh import Barh
+from ..charts.stacked_bar import StackedBar
+from ..charts.donut import Donut
+from ..charts.pie import Pie
+from ..charts.line import Line
+from ..charts.scatter import Scatter
